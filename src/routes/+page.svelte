c28990b27f785c0808001c4b28a9751e0b68e19b
zycoding <zycoding@gmail.com>
2026-01-13 01:20:17 -0500

    Add intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>This is a Git Release Branch Demo</h1>
+<div>
+  <p>This demo shows how to use Git release branches to manage deployments.</p>
+</div>
